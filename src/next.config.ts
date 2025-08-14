e94622bcea3be4fae7a61f31bc5c8b96f7da17b0
Patrick Marcus <patrickmarcus580@gmail.com>
2025-08-14 17:18:40 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/next.config.ts
+++ src/next.config.ts
@@ -4,11 +4,6 @@ import type {NextConfig} from 'next';
 const nextConfig: NextConfig = {
   output: 'export',
   /* config options here */
-  experimental: {
-    serverActions: {
-      enabled: false,
-    },
-  },
   webpack: (config, { isServer }) => {
     if (!isServer) {
       // Don't resolve 'async_hooks' on the client
