2d93efd1374765030b6a04fa60dee4dc4b51123f
Patrick Marcus <patrickmarcus580@gmail.com>
2025-08-14 14:38:19 +0000

    now pls make sure this site is structure in a way in can be hosted on gi

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@
 import type {NextConfig} from 'next';
 
 const nextConfig: NextConfig = {
+  output: 'export',
   /* config options here */
   allowedDevOrigins: ["6000-firebase-studio-*.cloudworkstations.dev"],
   reactStrictMode: false,
